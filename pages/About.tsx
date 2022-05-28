3bcc9df7024028c048b32ed5fe9cee019acfcd01
Joanna Huang <joanna.huang@borngroup.com>
2022-05-27 22:16:02 -0400

    [Phase1]: add frame back in

--- pages/About.tsx
+++ pages/About.tsx
@@ -82,15 +82,15 @@ const ImageContainer = styled.div`
   z-index: 5;
 `;
 
-// const ImageFrame = styled.div`
-//   border: 1px solid red;
-//   position: absolute;
-//   top: 1300px;
-//   right: 130px;
-//   width: 21.875rem;
-//   height: 31.25rem;
-//   z-index: 1;
-// `;
+const ImageFrame = styled.div`
+  border: 1px solid red;
+  position: absolute;
+  top: 1300px;
+  right: 130px;
+  width: 21.875rem;
+  height: 31.25rem;
+  z-index: 1;
+`;
 
 const TechList = styled.div`
   margin-top: 2rem;
